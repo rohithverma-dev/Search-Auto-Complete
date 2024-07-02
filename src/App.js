d5279c534f58847edb744fe30051b7449fba816f
rohit verma <rohithverma.dev@gmail.com>
2024-07-02 08:39:55 +0530

    CSS added

--- src/App.js
+++ src/App.js
@@ -23,14 +23,31 @@ function App() {
       id: 5,
       name: "radhika",
     },
+    {
+      id: 6,
+      name: "sarman",
+    },
+    {
+      id: 7,
+      name: "satyam",
+    },
+    {
+      id: 8,
+      name: "shubham",
+    },
+    {
+      id: 9,
+      name: "sahil",
+    },
+    {
+      id: 10,
+      name: "sarthak",
+    },
   ];
 
   const [name, setName] = useState("")
   const [newArr, setNewArr] = useState([])
 
-  const handleInput = (e)=>{
-    setName(e.target.value)
-  }
 
   useEffect(() => {
     setNewArr( prev => name_arr.filter( n=>  (name !== "") && (n.name).includes(name)  ) )
@@ -39,14 +56,14 @@ function App() {
 
   return (
    <>
+   <h1>Search Auto Complete</h1>
    <div className="container">
      <input type="text" value={name} placeholder="Search For Name" onChange={(e)=>{setName(e.target.value)}} />
-     <div className="box">
-     { newArr.map((ele)=>{
+     { newArr.length && <div className="box">
+     {newArr.map((ele)=>{
         return <div key={ele.id} onClick={()=>setName(ele.name)} className="name">{ele.name}</div>
       })}
-     
-     </div>
+     </div>}
    </div>
 
    </>
